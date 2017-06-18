23b1839369f33984b6a8f386e2b3ffcb0acea235
Tatiana Bercht <tatianabercht@tatianas-mbp.home>
2017-06-18 17:15:00 +0100

    1 test

---
+++ __tests__/lib/array-helpers.test.js
@@ -0,0 +1,9 @@
+import { sortFunction } from '../../src/lib/array-helpers'
+
+describe('Array helpers', () => {
+    describe('sort by', () => {
+        it('sorts by title', () => {
+            console.log('hello')
+        })
+    })
+})
